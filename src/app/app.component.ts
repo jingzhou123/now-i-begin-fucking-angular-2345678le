822fe4fbf87b7a47ccecf0cc22540dd947fd39ae
Your Name <you@example.com>
2019-07-07 01:45:11 -0700

    show ps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-test';
+export class AppComponent implements OnInit {
+  products = [
+    {
+      id: 1
+    }
+  ];
+  ngOnInit(): void {
+    // throw new Error("Method not implemented.");
+  }
 }
